01cf8a29593226cf2269413319c16dcaa0a3968c
marianocabezas <mcabezas@silver.udg.edu>
2020-07-13 19:36:46 +1000

    - changed datasets

--- datasets.py
+++ datasets.py
@@ -25,94 +25,21 @@ class Cropping2DDataset(Dataset):
         )
         if filtered:
             self.patch_slices = [
-                [s for s in slices_i if np.sum(label[s]) > 0]
-                for label, slices_i in zip(self.labels, slices)
+                (s, i) for i, (label, slices_i) in enumerate(zip(self.labels, slices))
+                for s in slices_i
+                if np.sum(label[(slice(None, None),) + s]) > 0
             ]
         else:
-            self.patch_slices = slices
-
-        self.max_slice = np.cumsum(list(map(len, self.patch_slices)))
-
-    def __getitem__(self, index):
-        # We select the case
-        case_idx = np.min(np.where(self.max_slice > index))
-
-        slices = [0] + self.max_slice.tolist()
-        patch_idx = index - slices[case_idx]
-        case_slices = self.patch_slices[case_idx]
-
-        # We get the slice indexes
-        none_slice = (slice(None, None),)
-        slice_i = case_slices[patch_idx]
-
-        inputs = self.data[case_idx][none_slice + slice_i].astype(np.float32)
-
-        labels = self.labels[case_idx][slice_i].astype(np.uint8)
-        target = np.expand_dims(labels, 0)
-
-        return inputs, target
-
-    def __len__(self):
-        return self.max_slice[-1]
-
-
-class CroppingDown2DDataset(Dataset):
-    def __init__(
-            self,
-            data, labels, patch_size=32, overlap=16, filtered=False,
-            ratio=10
-    ):
-        # Init
-        data_shape = data[0].shape
-        dim = len(data_shape)
-        self.data = [
-            imresize(
-                im, (im.shape[0],) + tuple(
-                    [length // ratio for length in im.shape[1:]]
-                ),
-                order=2
-            )
-            for im in data
-        ]
-        self.labels = [
-            torch.max_pool2d(
-                torch.tensor(np.expand_dims(lab, 0)).type(torch.float32), ratio
-            ).numpy().astype(np.bool)
-            for lab in labels
-        ]
-
-        if type(patch_size) is not tuple:
-            patch_size = (patch_size,) * dim
-        self.patch_size = patch_size
-        self.overlap = overlap
-
-        slices = get_slices_bb(
-            [np.squeeze(lab) for lab in self.labels],
-            self.patch_size, self.overlap
-        )
-        if filtered:
             self.patch_slices = [
-                [s for s in slices_i if np.sum(
-                    label[(slice(None, None),) + s]
-                ) > 0]
-                for label, slices_i in zip(self.labels, slices)
+                (s, i) for i, slices_i in enumerate(slices) for s in slices_i
             ]
-        else:
-            self.patch_slices = slices
-
-        self.max_slice = np.cumsum(list(map(len, self.patch_slices)))
 
     def __getitem__(self, index):
         # We select the case
-        case_idx = np.min(np.where(self.max_slice > index))
-
-        slices = [0] + self.max_slice.tolist()
-        patch_idx = index - slices[case_idx]
-        case_slices = self.patch_slices[case_idx]
+        case_idx, slice_i = self.patch_slices[index]
 
         # We get the slice indexes
         none_slice = (slice(None, None),)
-        slice_i = case_slices[patch_idx]
 
         inputs = self.data[case_idx][none_slice + slice_i].astype(np.float32)
 
@@ -124,4 +51,29 @@ class CroppingDown2DDataset(Dataset):
         return inputs, target
 
     def __len__(self):
-        return self.max_slice[-1]
+        return len(self.patch_slices)
+
+
+class CroppingDown2DDataset(Cropping2DDataset):
+    def __init__(
+            self,
+            data, labels, patch_size=32, overlap=16, filtered=False,
+            ratio=10
+    ):
+        # Init
+        downdata = [
+            imresize(
+                im, (im.shape[0],) + tuple(
+                    [length // ratio for length in im.shape[1:]]
+                ),
+                order=2
+            )
+            for im in data
+        ]
+        downlabels = [
+            torch.max_pool2d(
+                torch.tensor(np.expand_dims(lab, 0)).type(torch.float32), ratio
+            ).numpy().astype(np.bool)
+            for lab in labels
+        ]
+        super().__init__(downdata, downlabels, patch_size, overlap, filtered)

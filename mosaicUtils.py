0b2a3b65a6504fb350aed33f239e7f1e57d59aaa
x <y>
2021-10-18 16:30:53 +0900

    mosaicUtils.py

--- mosaicUtils.py
+++ mosaicUtils.py
@@ -1,78 +1,106 @@
+# file to read a mosaic file and annotations files and divide them along with ROI and treetops into smaller versions
 import numpy as np
 import sys
 import dice
 import cv2
 import os
 
-def mergeMasks(mask1,mask2):
-    if mask1 is None:mask1=mask2.copy()
-    else:mask1[mask2<10]=0
-    return mask1
+def subdivideImage(image,siteName,outputDir,subdiv):
+    #print("subdividing ")
+    #print(image)
+    #print(siteName)
+    #print(subdiv)
 
-def main(argv):
+    fullImage=cv2.imread(image[1])
 
-    paramsFile=argv[1]
-    with open(paramsFile) as fp:
-        line = fp.readline()
-        cnt = 1
-        while line:
-            if line[:4]=="FUSE": #ignore comments and anything not startig with FUSE
-                #print("Line {}: {}".format(cnt, line.strip()))
-                lineSplit=line.split(" ")
-                dataPath=lineSplit[1]
-                outMaskName=lineSplit[2]
-                outMask=None
-                for i in range(3,len(lineSplit)):
-                    currentMaskName=os.path.join(dataPath,lineSplit[i].strip())
-                    #print(currentMaskName)
-                    currentMask=cv2.imread(currentMaskName)
-                    if currentMask is None: raise Exception("Not found mask "+currentMaskName)
-                    outMask=mergeMasks(outMask,currentMask)
+    for i,s in enumerate(subdiv):
+        dirName=outputDir+"/"+siteName+"Div"+str(i)+"/"
+        newImage=fullImage[s[0]:s[1],s[2]:s[3]]
+        newSiteName=siteName+"Div"+str(i)
+        newImageName=dirName+newSiteName+image[0]+".jpg"
+        #print(newImageName)
+        cv2.imwrite(newImageName,newImage)
 
-            line = fp.readline()
-            cnt += 1
+def processSite(folder,siteName,spList,nSteps,outFolder):
+    fileList=os.listdir(folder)
+    #print(fileList)
 
-    cv2.imwrite(os.path.join(dataPath,outMaskName+".jpg"),outMask)
+    roiFile=folder+"/"+siteName+"ROI.jpg"
+    roi=cv2.imread(roiFile,cv2.IMREAD_GRAYSCALE)
+    if roi is None:raise Exception("NO ROI found in site "+roiFile+"")
+    roi[roi>50]=255
+    roi[roi<50]=0
 
+    treeTopsFile=folder+"/"+siteName+"Treetops_ROI.jpg"
+    if treeTopsFile is None:raise Exception("NO treetopsROI file found in site "+treeTopsFile+"")
 
-    sys.exit()
-
+    mosaicFile=folder+"/"+siteName+".jpg"
+    if mosaicFile is None:raise Exception("NO mosaic file found in site "+treeTopsFile+"")
 
 
+    # Find contours
+    cnts = cv2.findContours(255-roi, cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)
+    cnts = cnts[0] if len(cnts) == 2 else cnts[1]
 
-    gtMask=cv2.imread(argv[1],cv2.IMREAD_GRAYSCALE).astype("uint8")
-    if gtMask is None: raise Exception("No ground Truth mask read")
+    # Find extreme coordinates of ROI
+    cnt=cnts[0]
+    x,y,w,h = cv2.boundingRect(cnt)
+
+    xStep=int(w/nSteps)
+    yStep=int(h/nSteps)
+
+    subdiv=[]
+    for i in range(nSteps):
+        for j in range(nSteps):
+            #roiPart=roi[y+j*yStep:y+(j+1)*yStep,x+i*xStep:x+(i+1)*xStep]
+            subdiv.append((y+j*yStep,y+(j+1)*yStep,x+i*xStep,x+(i+1)*xStep))
+
+            #print(str(i)+" "+str(j))
+            #cv2.imwrite(str(i)+str(j)+"shit.png",roiPart)
+
+    #now that we have the subdivisions that we want to make, add all the images that we want to sudvivide
+    imagesToSubdivide=[("ROI",roiFile),("Treetops_ROI",treeTopsFile),("",mosaicFile)]
+    for i in range(len(spList)):
+        #print("label "+str(i))
+        currentMaskFile=folder+"/"+siteName+spList[i]+".jpg"
+        currentMask=cv2.imread(currentMaskFile,cv2.IMREAD_GRAYSCALE)
+        if currentMask is None:
+            #print("species "+spList[i]+" not present in site "+siteName)
+            pass
+        else: imagesToSubdivide.append((spList[i],currentMaskFile))
+
+    #print(imagesToSubdivide)
+    for i,s in enumerate(subdiv):
+        dirName=outFolder+"/"+siteName+"Div"+str(i)+"/"
+        #print("should be making dir "+dirName)
+        try:
+            # Create target Directory
+            os.mkdir(dirName)
+            print("Directory " , dirName ,  " Created ")
+        except FileExistsError:
+            print("Directory " , dirName ,  " already exists")
 
 
-    mask=cv2.imread(argv[2],cv2.IMREAD_GRAYSCALE).astype("uint8")
-    if mask is None: raise Exception("No coarse mask read")
-
-    #print(np.unique(mask))
-
-
-    code=int(argv[3])
-    numClasses=46
-
-    if code==0:# Evaluate Dice coefficient
-        result=dice.dice(mask,gtMask )
-        print("*******************************************  dice: "+str(result))
-    elif code==1:# Evaluate percentage of matched pixels (includes Background)
-        result=dice.equalValue(mask,gtMask,True )
-        print("*******************************************  % Pixels with equal value (inlcudes BKG): "+str(result))
-    elif code==2:# Evaluate percentage of matched pixels (not including Background)
-        result=dice.equalValue(mask,gtMask,False )
-        print("*******************************************  % Pixels with equal value (no BKG): "+str(result))
-    elif code==3:# Evaluate percentage of TPR for each class
-        for i in range(numClasses+1):
-            result=dice.RecallLabelI(gtMask.copy(),mask.copy(),i)
-            print("******************************************* Class "+str(i)+"  Recall "+str(result))
-    elif code==4:# Evaluate percentage of TPR for each class
-        for i in range(numClasses+1):
-            result=dice.PrecisionLabelI(gtMask.copy(),mask.copy(),i)
-            print("******************************************* Class "+str(i)+"  Precision: "+str(result))
-
-
-    return result
+    for image in imagesToSubdivide:
+        subdivideImage(image,siteName,outFolder,subdiv)
 
 if __name__ == '__main__':
-    main(sys.argv)
+
+    # Read a directory that contains sites and another for the output
+    inputDir=sys.argv[1]
+    outDir=sys.argv[2]
+    subdivisions=int(sys.argv[3]) # must be power of 2
+
+    maxSpeciesCode=46
+    speciesDict={}
+    speciesList=["S"+'{:02d}'.format(i) for i in range(0,maxSpeciesCode+1)]
+    for sp in range(len(speciesList)):speciesDict[speciesList[sp]]=sp
+
+    # for all the folder in the inDIr, consider all annotation files, the ROI file and the treetop files. 1) Compute bounding box of the ROI
+    # 2) break everything into "n" parts according to the ROI bounding box and store in the output folder
+    siteFolders=sorted(os.listdir(inputDir))
+    print(siteFolders)
+
+    for x in siteFolders:
+        processSite(inputDir+x,x,speciesList,int(subdivisions/2),outDir)
+        sys.exit()

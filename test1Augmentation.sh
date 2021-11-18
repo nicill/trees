378a66448dbbb6baecd92fc2b50da1cadcd3bbee
x <y>
2021-11-18 14:09:18 +0900

    imporved test1

--- test1Augmentation.sh
+++ test1Augmentation.sh
@@ -7,32 +7,34 @@ conversion=./speciesConversionTableLuca.txt
 softDir=$(pwd)
 
 #try for different augmentations and decreases
-for a in 0 2 5 10
+for a in 0 2 4 5 8 10 20
 do
-  for d in 0 20 50 70
+  for d in 0 10 20 50 70 90
   do
-    for th in 0 0.5 0.7 0.9
+    for th in 0 25 50 70 80 90 95
     do
+        echo "^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^starting $a $d $th"
+        date
         CUDA_VISIBLE_DEVICES=$GPU;python $softDir/tree_detection.py -d $dataDir -e 20 -labFus $conversion -aug $a -dec $d -th $th
     done
   done
 done
 
-exit()
+#exit()
 
-code=$2
-th=$3
+#code=$2
+#th=$3
 
 #make a file for every output (decided perc, accuracy, classwise(prec,rec,dice) )
 
-echo " $dataDir"
+#echo " $dataDir"
 
-cd $dataDir
+#cd $dataDir
 
-for f in *;
-do
-    if [ -d "$f" ]; then
+#for f in *;
+#do
+#    if [ -d "$f" ]; then
 	#echo " $f is a directory"
-	python $softDir/evaluateSegmentationResults.py $dataDir/$f/$f"GT.png" $dataDir/$f/$f"ResultTH"$th".png" $code $dataDir/$f/$f"ROI.jpg"
-    fi
-done
+#	python $softDir/evaluateSegmentationResults.py $dataDir/$f/$f"GT.png" $dataDir/$f/$f"ResultTH"$th".png" $code $dataDir/$f/$f"ROI.jpg"
+#    fi
+#done
